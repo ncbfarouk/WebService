d6a23c66581f663d514cf5292483a14e7aaafa36
Farouk Necib <farouk.necib@devoteam.com>
2019-04-15 15:25:47 +0200

    WinForms

---
+++ TestProject/Form1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WinForm;
+namespace TestProject
+{
+    [TestClass]
+    public class Form1
+    {
+        [TestMethod]
+        public void CallFbinacciTest()
+        {
+            int expectedResult = 55;
+            WinForm.Form1 FabiForm= new WinForm.Form1();
+            Assert.AreEqual(expectedResult, FabiForm.CallFbinacci());
+        }
+    }
+}
---
+++ WinForm/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using Newtonsoft.Json;
+namespace WinForm
+{
+    public partial class Form1 : Form
+    {
+        static WsFab.WebServiceSoapClient FabWs = new WsFab.WebServiceSoapClient();
+       
+
+
+        Form progForm = new Form
+        {
+            Width = 600,
+            Height = 400,
+            MaximizeBox = false,
+            MinimizeBox = false,
+            FormBorderStyle = FormBorderStyle.None,
+            Text = "BusyForm",
+            StartPosition=FormStartPosition.CenterScreen,
+            BackColor=Color.DodgerBlue
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public int CallFbinacci()
+        {
+            System.Threading.Thread.Sleep(2000); 
+            return FabWs.Fibonacci(10);
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+            var task1 = Task.Run(new Action(() =>
+            {
+                int x = CallFbinacci();
+                MessageBox.Show(x.ToString());
+
+                progForm.Invoke(new MethodInvoker(delegate ()
+                            {
+                                progForm.Close();
+                            }
+
+                    ));
+
+            }
+                ));
+
+            progForm.ShowDialog();
+
+
+        }
+
+       
+    }
+}
